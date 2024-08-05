71b97b71af78873e84e8a9008cb386c557ef85ac
Ar13l-x07 <ar13l.x07@gmail.com>
2024-08-04 23:29:52 -0400

    [format/format.h] - Add unit tests comments to format.h

--- format/format.h
+++ format/format.h
@@ -228,3 +228,42 @@ namespace jlo
 
 
 }
+
+
+
+
+
+
+
+
+/* Unit Tests:
+
+std::string a = "a";
+const char* b = "b";
+int c = 0;
+long d = 1;
+long long e = 2;
+unsigned f = 3;
+unsigned long g = 4;
+unsigned long long h = 5;
+float i = 6;
+double j = 7;
+long double k = 8;
+	
+	
+std::cout << jlo::format("a:{}\nb:{}\n0:{}\n1:{}\n2:{}\n3:{}\n4:{}\n5:{}\n6.000000:{}\n7.000000:{}\n8.000000:{}\n", a, b, c, d, e, f, g, h, i, j, k);
+
+// Expected Output:
+// a:a
+// b:b
+// 0:0
+// 1:1
+// 2:2
+// 3:3
+// 4:4
+// 5:5
+// 6.000000:6.000000
+// 7.000000:7.000000
+// 8.000000:8.000000
+
+*/
